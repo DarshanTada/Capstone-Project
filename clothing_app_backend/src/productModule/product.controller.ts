c4d2eecbb57e11887629a413421c6ce298fce0e6
Adeesh Patel <ajpatel@conestogac.on.ca>
2025-07-02 18:52:46 -0400

    update user register api

--- clothing_app_backend/src/productModule/product.controller.ts
+++ clothing_app_backend/src/productModule/product.controller.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction, RequestHandler } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import Product from '../productModule/product.model';
 import ProductVariant from '../productModule/productVariant.model';
 import ProductImage from '../productModule/productImage.model';
 import CareInstruction from '../productModule/careInstruction.model';
-import Festival from '../productModule/festival.model';
+import Festival from '../festivalModule/festival.model';
 import Season from '../productModule/season.model';
 import dotenv from "dotenv";
 
