9ab4a3591153a05d17a4a7fd7a90cff4a4124cb6
Adeesh Patel <ajpatel@conestogac.on.ca>
2025-07-24 18:52:17 -0400

    resolve catewgory API call issue

--- clothing_app_frontend/lib/main.dart
+++ clothing_app_frontend/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:clothing_app_frontend/authModule/screens/verify_otp_screen2.dart
 import 'package:clothing_app_frontend/chatbotModule/screens/chatbot_screen.dart';
 import 'package:clothing_app_frontend/common_functions.dart';
 import 'package:clothing_app_frontend/homeModule/provider/category_provider.dart';
-import 'package:clothing_app_frontend/homeModule/screens/category_screen.dart';
+// import 'package:clothing_app_frontend/homeModule/screens/category_screen.dart';
 import 'package:clothing_app_frontend/homeModule/screens/home_screen.dart';
 import 'package:clothing_app_frontend/homeModule/widgets/size_chart_screen.dart';
 import 'package:device_info_plus/device_info_plus.dart';
