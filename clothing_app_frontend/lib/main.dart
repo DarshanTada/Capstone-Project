1c1bce9ff9793e9a79ab1997a4086b7e3c5cf613
Adeesh Patel <ajpatel@conestogac.on.ca>
2025-07-31 18:42:54 -0400

    remove unwanted things

--- clothing_app_frontend/lib/main.dart
+++ clothing_app_frontend/lib/main.dart
@@ -1,17 +1,8 @@
-// ignore_for_file: depend_on_referenced_packages
-
 import 'dart:io';
-
-import 'package:clothing_app_frontend/authModule/screens/intro_screen_1.dart';
-import 'package:clothing_app_frontend/authModule/screens/login.dart';
-import 'package:clothing_app_frontend/authModule/screens/phone_number_screen.dart';
-import 'package:clothing_app_frontend/categoryModule/screens/category.dart';
 import 'package:clothing_app_frontend/chatbotModule/screens/chatbot_screen.dart';
 import 'package:clothing_app_frontend/common_functions.dart';
 import 'package:clothing_app_frontend/homeModule/provider/category_provider.dart';
-
 import 'package:clothing_app_frontend/addressModule/provider/address_provider.dart';
-
 import 'package:clothing_app_frontend/homeModule/provider/home_provider.dart';
 import 'package:clothing_app_frontend/authModule/providers/logout_provider.dart';
 
@@ -33,7 +24,6 @@ import 'package:provider/provider.dart';
 import 'authModule/providers/auth_provider.dart';
 import 'navigation/navigation_service.dart';
 import 'firebase_options.dart';
-import 'test_screen.dart';
 
 final LocalStorage storage = LocalStorage('re_household');
 
